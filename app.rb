282013c6e6d91043fb9270d4ad896d0ec461b7ff
Andy Read <andyreadpnw@gmail.com>
2019-10-08 13:57:57 -0700

    Done.

--- app.rb
+++ app.rb
@@ -8,11 +8,16 @@ class App < Sinatra::Base
   end
 
   # This is a sample dynamic route.
-  get "/hello/:name" do
+  get "/goodbye/:name" do
     @user_name = params[:name]
-    "Hello #{@user_name}!"
+    "Goodbye, #{@user_name}."
   end
 
-  # Code your final two routes here:
+  get '/multiply/:num1/:num2' do
+    @num1 = params[:num1]
+    @num2 = params[:num2]
+    @product = @num1.to_i * @num2.to_i
+    "#{@product}"
+  end
 
 end
