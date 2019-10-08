1d6a5ab80150338f0239a13d873c25d3cd822fbf
Leoncodes00 <chachaiyang3@gmail.com>
2019-10-08 13:47:40 -0700

    Done.

--- app.rb
+++ app.rb
@@ -11,8 +11,13 @@ class App < Sinatra::Base
   get "/hello/:name" do
     @user_name = params[:name]
     "Hello #{@user_name}!"
+    erb :hello
   end
-
   # Code your final two routes here:
+  get '/goodbye/:name' do
+    person_name = params[:name]
+    "Goodbye #{person_name}"
+    erb :/goodbye/show.html
+  end
 
 end
